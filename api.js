9107c7181e1c1ddf41f3d861da7fc7d7218dbbd3
Chokseys2022 <choksey2022@gmail.com>
2024-02-11 19:59:06 -0800

    cleaning code

--- api.js
+++ api.js
@@ -1,43 +1,43 @@
-// Define the Spoonacular API key
+//api.js: Contains functions for fetching recipe data from the Spoonacular API.
+//*****************************************************************************
+
+// Defining Spoonacular API key
 const apiKey = "83391e00768640b29c383a671ab06380";
 // Define the base URL for the Spoonacular API
 const baseUrl = "https://api.spoonacular.com";
 
-// Asynchronous function to fetch recipes
+// Async funct to fetch recipes
 export async function getRecipe(query) {
-  // Construct the URL for fetching recipes
+  // url to fetch recipes
   const url = `${baseUrl}/recipes/complexSearch?apiKey=${apiKey}&query=${query}`;
-  // Fetch data from the constructed URL
+  // Fetch data from url
   const response = await fetch(url);
-  // Check if the response is not OK
   if (!response.ok) {
-    // Throw an error if response is not OK
     throw new Error(`HTTP error! status: ${response.status}`);
   }
   // Return JSON data
   return response.json();
 }
 
-// Asynchronous funct for POST request
+//Async funct for POST request
 
 export async function postRequest(data) {
   const url = `${baseUrl}/api.spoonacular.com/recipes/complexSearch`;
   // Fetch data
   const response = await fetch(url, {
-    // Set request method to POST
+    //Set request method to POST
     method: "POST",
-    // Define request headers
+    //request headers
     headers: {
-      // Set content type to JSON
+      //set content type to JSON
       "Content-Type": "application/json",
     },
-    // Convert data to JSON string
     body: JSON.stringify(data),
   });
-
   if (!response.ok) {
     throw new Error(`HTTP error! status: ${response.status}`);
   }
-  // Return JSON data
+  //return JSON data
   return response.json();
 }
+// **********************************END CODE***********************************//
